d80b226efa17aab47bf012729854ddf8839ac8c2
juanmanoel <37448831+juanengml@users.noreply.github.com>
2020-08-23 17:29:20 -0300

    Create sql-create-db-insertdata.sql

---
+++ sql-create-db-insertdata.sql
@@ -0,0 +1,103 @@
+create database TechLogica_HML;
+
+use TechLogica_HML;
+
+CREATE TABLE `departamento` (
+  `id_departamento` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nome` varchar(255)
+);
+
+CREATE TABLE `projeto` (
+  `id_projeto` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nome` varchar(255),
+  `data_inicio` datetime,
+  `id_departamento` integer
+);
+
+CREATE TABLE `funcionario` (
+  `id_funcionario` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `cpf` integer,
+  `nome` varchar(255),
+  `sexo` varchar(255),
+  `data_nascimento` date,
+  `salario` float,
+  `id_projeto` integer,
+  `id_departamento` integer
+);
+
+CREATE TABLE `dependentes` (
+  `id_dependentes` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nome` varchar(255),
+  `sexo` varchar(255),
+  `id_funcionario` integer
+);
+
+ALTER TABLE `projeto` ADD CONSTRAINT `Departameno_has_projeto` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id_departamento`);
+
+ALTER TABLE `funcionario` ADD CONSTRAINT `Projeto_has_funcionario` FOREIGN KEY (`id_projeto`) REFERENCES `projeto` (`id_projeto`);
+
+ALTER TABLE `funcionario` ADD CONSTRAINT `Gerencia_Has_funcionario` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id_departamento`);
+
+ALTER TABLE `dependentes` ADD CONSTRAINT `funcionario_has_dependente` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionario` (`id_funcionario`);
+
+-- insert departamentos 
+ 
+	insert into departamento (nome) values ("Financas");
+	insert into departamento (nome) values ("RH");
+	insert into departamento (nome) values ("Tecnologia");
+	insert into departamento (nome) values ("Dados");
+	insert into departamento (nome) values ("Engenharia");
+
+--DELETE FROM `projeto`;
+-- insert projetos 
+
+	insert into projeto (nome, data_inicio, id_departamento) values ("WEB APP FORECAST PUBLICITARIO - TECH TEAM",Now(), 3);
+	insert into projeto (nome, data_inicio, id_departamento) values ("MODELAGEM FORECAST PUBLICITARIO - DATA TEAM",Now(), 4);
+	insert into projeto (nome, data_inicio, id_departamento) values ("EXTRACAO FORECAST PUBLICITARIO - ENG DATA TEAM",Now(), 5);
+
+-- insert funcionarios 
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (123123,"Juan Nascimento Padilha","Masculino", '2000-10-06', 5000,1,3);
+
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (987623,"Tatiana da Pinha","Feminino", '1999-10-06', 5000,1,3);
+
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (457564,"Howard Stark","Feminino", '1998-12-08', 6000,1,3);
+
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (545614,"Matias Nascimento","Masculino", '2000-10-06', 5000,2,3);
+
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (756734,"Larissa Vieira","Feminino", '2003-01-06', 5000,2,3);
+
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (123678,"Debora dos Santos","Feminino", '2005-11-08', 5000,2,3);
+	
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (154562,"Carolina Siqueira","Feminino", '2001-10-01', 3000,3,4);     
+
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (165562,"Juliana KarEll","Feminino", '2001-06-01', 9000,3,4);     
+	     
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (653457,"Claudia Newell","Feminino", '1996-10-04', 10000,3,4);     
+	     
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (445677,"Newell Duarte","Masculino", '1997-11-04', 1000,1,1);     
+	     
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (456123,"Cristofer de Alencar","Masculino", '1997-12-04', 5000,1,1);  
+	        
+	insert into funcionario (cpf,nome,sexo, data_nascimento,salario, id_projeto,id_departamento)
+	     values (235600,"Alexandre Duarte Pinheiro","Masculino", '1997-12-04', 5000,1,1);     
+
+--- insert dependentes
+
+insert into dependentes(nome,sexo,id_funcionario)
+
+values ("Duarte Jr","Masculino",12),
+	("Roger Siqueira","Masculino",11),
+	("Francisca Divina","Feminina",10),
+	("JoseMar Lucio","Masculino",5),
+	("Ada Byron","Feminino",1);
